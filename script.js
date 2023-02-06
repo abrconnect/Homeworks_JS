19001e4e0b43d110aa587001d74ccac9ae88c5df
abrconnect <abramtsov.andrey@gmail.com>
2023-02-06 22:52:01 +0200

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+//1
+// Дано масив виду
+// наповнити масив res значеннями з масиву arr з типом number ( “1” ⇒ 1 ) використовуючи цикл
+
+// const arr = ["1", "2", "4", "7"];
+// const res = [];
+// for (let i = 0; i < arr.length; i++) {
+//     res.push(parseInt(arr[i]));
+// }
+
+// console.log(res);
+
+//2 Дано масив виду
+// змінити існуючий масив на масив виду [”P”, “U”, “U”] 
+//(залишити по першій літері з кожного слова)
+
+// const arr1 = ["Poland", "Ukraine", "USA"];
+// const res = [];
+// for (let item of arr1) {
+//     res.push(item[0]);
+// }
+// console.log(res)
+
+//3 
+//Дано масив виду
+// знайти індекс елемента “Kyiv”
+
+// const arr2 = ["Lviv", "Kyiv", "Poltava"];
+// const index = arr2.indexOf("Kyiv");
+// console.log(index)
