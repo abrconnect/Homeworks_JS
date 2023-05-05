6f6672872c84a05f6fce1e8281b2fd20a9410d79
abrconnect <abramtsov.andrey@gmail.com>
2023-05-05 23:44:00 +0300

    update: add task description

--- main.js
+++ main.js
@@ -37,3 +37,13 @@ const result = calculator
     .result();
 
 console.log(result);
+
+
+// опис завдання
+
+// Код з уроку  [https://github.com/SpaceHolderdJs/f-25/tree/users-search-way-2/JavaScript/Patterns](https://github.com/SpaceHolderdJs/f-25/tree/users-search-way-2/JavaScript/Patterns)
+
+// На базі коду з уроку відтворіть свою реалізацію патерну Chain-Responsibilities 
+// - Створіть будь-яку сутність (клас та його екземпляр)
+// - Опишіть 2-3 методи класу які підтримують Chain-Responsibilities концепцію
+// - Складність методів не оцінюється (можна просто виводити щось у консоль)
