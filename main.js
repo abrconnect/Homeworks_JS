39cce15ae5b2c07a0087bba4add90e6722f6b155
abrconnect <abramtsov.andrey@gmail.com>
2023-05-29 01:45:19 +0300

    done 3

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ const str2 = "some text for example";
 const regexp2 = /^[a-z\s]*$/;
 const result2 = regexp2.test(str2);
 console.log(result2, str2, "all elements are in lowerCase");
+
+// 3. Перевірка чи строка є паліндромом
+
+function isPalindrome(text) {
+    let reversed = text.split("").reverse().join("");
+    return text === reversed;
+}
+
+const text1 = "дід";
+const text2 = "око";
+console.log(`${text1} is palindrome ${isPalindrome(text1)}`);
+console.log(`${text2} is palindrome ${isPalindrome(text2)}`);
+
+
+
