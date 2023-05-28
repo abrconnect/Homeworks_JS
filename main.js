fda6c2240fa0df75020dfe67e6172bca33ddd1d0
abrconnect <abramtsov.andrey@gmail.com>
2023-05-29 00:27:26 +0300

    done 2

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-// 1. Всі елементи якої є не числами
+// 1. Всі елементи строки не є числами
 
 const str = "Some text for example";
 const regexp = /^\D*$/;
+// // const regexp = /^[^0-9]*$/; // <--- або
 
 const result = regexp.test(str);
-console.log(result);
+console.log(result, str, "all elements are not numbers");
 
+// 2. Всі елементи строки написані з маленької літери
+
+const str2 = "some text for example";
+const regexp2 = /^[a-z\s]*$/;
+const result2 = regexp2.test(str2);
+console.log(result2, str2, "all elements are in lowerCase");
